b16b4992a857c33f1467354fba3ea95507d0d92d
dionysoleao <dionysoleao@gmail.com>
2022-01-22 08:23:54 -0500

    busca atualizada

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from './Home.module.css'
+import Busca from '../components/Busca/busca'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <h2>yeah</h2>
+      <Busca />
+    </div>
+  )
+}
